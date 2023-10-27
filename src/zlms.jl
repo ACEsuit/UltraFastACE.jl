ee5bb762edffd84259642c5b3eef612c826444b9
ACEsuit <christohortner@gmail.com>
2023-10-26 17:08:19 -0700

    more cleanup

--- src/zlms.jl
+++ src/zlms.jl
@@ -1,10 +1,10 @@
 #
 # This provides generated real solid harmonics (and spherical harmonics) 
 # using the approach proposed by 
-#       Efficient Spherical Harmonic Evaluation, Peter-Pike Sloan, 
+#       [1] Efficient Spherical Harmonic Evaluation, Peter-Pike Sloan, 
 #       Journal of Computer Graphics Techniques, Vol. 2, No. 2, 2013
 # The ideas are expanded on and details are worked out very nicely in 
-#       Fast evaluation of spherical harmonics with sphericart, 
+#       [2] Fast evaluation of spherical harmonics with sphericart, 
 #       Filippo Bigi, Guillaume Fraux, Nicholas J. Browning and Michele Ceriotti
 #       arXiv:2302.08381; J. Chem. Phys. 159, 064802 (2023)
 # Some aspects of this implementation, specifically how to deal with the 
@@ -14,22 +14,25 @@
 # bases (maxL large) but we gain about a factor 2-3 for small bases 
 # in the range L = 3, 4, 5. 
 #
+# The current version of the code was written only with reference to 
+# the published paper [2] and without consulting the reference implementation 
+# provided by the authors. 
+#
+module SpheriCart
 
 using StaticArrays, OffsetArrays, StaticPolynomials
-using DynamicPolynomials: @polyvar
-using LoopVectorization: @avx, @turbo 
 
 """
 `sizeY(maxL):`
-Return the size of the set of spherical harmonics ``Y_{l,m}(θ,φ)`` of
+Return the size of the set of spherical harmonics ``Y_l^m`` of
 degree less than or equal to the given maximum degree `maxL`
 """
-sizeY(maxL) = (maxL + 1) * (maxL + 1)
+sizeY(maxL) = (maxL + 1)^2
 
 """
 `lm2idx(l,m):`
-Return the index into a flat array of real spherical harmonics `Y_lm`
-for the given indices `(l,m)`. `Y_lm` are stored in l-major order i.e.
+Return the index into a flat array of real spherical harmonics ``Y_l^m``
+for the given indices `(l,m)`. ``Y_l^m`` are stored in l-major order i.e.
 ```
 	[Y(0,0), Y(1,-1), Y(1,0), Y(1,1), Y(2,-2), ...]
 ```
@@ -38,7 +41,7 @@ lm2idx(l::Integer, m::Integer) = m + l + (l*l) + 1
 
 """
 Inverse of `lm2idx`: given an index into a vector of Ylm values, return the 
-`l, m` indices.
+`(l, m)` indices.
 """
 function idx2lm(i::Integer) 
 	l = floor(Int, sqrt(i-1) + 1e-10)
@@ -46,8 +49,10 @@ function idx2lm(i::Integer)
 	return l, m 
 end 
 
-
-function _gen_Flm(L::Integer)
+"""
+Generates the `F[l, m]` values exactly as described in [2]. 
+"""
+function generate_Flms(L::Integer)
    Flm = OffsetMatrix(zeros(L+1, L+1), (-1, -1))
    for l = big(0):big(L)
       for m = big(0):big(l)
@@ -59,86 +64,18 @@ function _gen_Flm(L::Integer)
 end
 
 
-
-function _Zlms(L::Integer, rr::AbstractVector{T}) where {T}
-   @assert length(rr) == 3
-   Flm = _gen_Flm(L)
-   len = sizeY(L)
-   rt2 = sqrt(2) 
-
-   s = OffsetVector(Vector{Any}(undef, L+1), (-1,))
-   c = OffsetVector(Vector{Any}(undef, L+1), (-1,))
-   Q = OffsetMatrix(Matrix{Any}(undef, (L+1, L+1)), (-1, -1))
-   Z = OffsetMatrix(Matrix{Any}(undef, (L+1, 2*L+1)), (-1, -L-1))
-   
-   x = rr[1]   
-   y = rr[2]
-   z = rr[3]
-   r² = x^2 + y^2 + z^2
-   
-   # c_m and s_m 
-   s[0] = zero(T)
-   c[0] = one(T) 
-   for m = 1:L 
-      s[m] = s[m-1] * x + c[m-1] * y
-      c[m] = c[m-1] * x - s[m-1] * y
-   end
-   # change c[0] to 1/rt2 to avoid a special case l-1=m=0 later 
-   c[0] = one(T)/rt2
-   
-   Q[0,0] = one(T)
-   Z[0,0] = (Flm[0,0]/rt2) * Q[0,0]
-
-   for l = 1:L 
-      # Q_l^l and Y_l^l
-      # m = l 
-      Q[l,l] = - (2*l-1) * Q[l-1,l-1]
-      Z[l,l] = Flm[l,l] * Q[l,l] * c[l]
-      Z[l,-l] = Flm[l,l] * Q[l,l] * s[l]
-      # Q_l^l-1 and Y_l^l-1
-      # m = l-1 
-      Q[l,l-1] = (2*l-1) * z * Q[l-1, l-1]
-      Z[l,-l+1] = Flm[l,l-1] * Q[l,l-1] * s[l-1]
-      Z[l,l-1] = Flm[l,l-1] * Q[l,l-1] * c[l-1]  # overwrite if m = 0 -> ok 
-      # now we can go to the second recursion 
-      for m = l-2:-1:0 
-         Q[l,m] = (2*l-1) * z * Q[l-1,m] - (l+m-1) * r² * Q[l-2,m]
-         Z[l,-m] = Flm[l,m] * Q[l,m] * s[m]
-         Z[l,m] = Flm[l,m] * Q[l,m] * c[m]
-      end
-   end
-
-   # finally generate an svector output 
-   return [ getindex(Z, idx2lm(i)...) for i = 1:len ]
-end
-
-
-function generate_Zlms(L::Integer)
-   @polyvar rr[1:3]  
-   dynamic_system = _Zlms(L, rr)
-   # a little trick to convert some terms to actual polynomials 
-   dynamic_system = dynamic_system .+ (0*rr[1])
-   # convert to a static system 
-   return PolynomialSystem(dynamic_system)
-end
-
-
-
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#     BACKUP 
-# The code below is a start of an explicitly generated code that 
-# might be faster, and an approach like that might in particular 
-# be faster for backpropagation. 
+# Implementation 1: 
+#  This is a generated code for a single input and maximal performance
+#  (with a single input) 
+#  
 
 function _codegen_Zlm(L, T) 
-   Flm = _gen_Flm(L)
+   Flm = generate_Flms(L)
    len = sizeY(L)
    rt2 = sqrt(2) 
 
    code = Expr[] 
-   push!(code, :(x = rr[1]))
-   push!(code, :(y = rr[2]))
-   push!(code, :(z = rr[3]))
    push!(code, :(r² = x^2 + y^2 + z^2))
 
    # c_m and s_m 
@@ -178,12 +115,15 @@ function _codegen_Zlm(L, T)
                 join( ["Z_$i, " for i = 1:len], ) * ")"))
 end
 
-@generated function Zlms(::Val{L}, rr::SVector{3, T}) where {L, T <: AbstractFloat}
+@generated function static_solid_harmonics(::Val{L}, x::T, y::T, z::T) where {L, T <: AbstractFloat}
    code = _codegen_Zlm(L, T)
    return quote
       $(Expr(:block, code...))
    end
 end
+
+zlms(valL::Val{L}, rr::SVector{3, T}) where {L, T} = 
+      zlms(valL, rr[1], rr[2], rr[3])
 
 
 # ------------------------------------ 
@@ -195,46 +135,86 @@ struct ZlmBasis{L, T1}
 end
 
 function ZlmBasis(L::Integer) 
-   Flm = _gen_Flm(L)
+   Flm = generate_Flms(L)
    ZlmBasis{L, eltype(Flm)}(Flm, ArrayPool(FlexArrayCache))
 end
 
-# _get_temps(basis::ZlmBasis{L}, nX, T) where {L} = 
-#    acquire!(basis.cache, :x,  (nX, ),    T), 
-#    acquire!(basis.cache, :y,  (nX, ),    T), 
-#    acquire!(basis.cache, :z,  (nX, ),    T), 
-#    acquire!(basis.cache, :r2, (nX, ),    T), 
-#    acquire!(basis.cache, :s,  (nX, L+1), T),
-#    acquire!(basis.cache, :c,  (nX, L+1), T),
-#    acquire!(basis.cache, :Q,  (nX, sizeY(L)), T)
+function evaluate(basis::ZlmBasis{L}, 
+                  Rs::AbstractVector{SVector{3, T}}) where {L, T}
+   Z = zeros(length(Rs), sizeY(L))
+   evaluate!(Z, basis, Rs)
+   return Z
+end
 
 function evaluate!(Z::AbstractMatrix, 
                    basis::ZlmBasis{L}, 
                    Rs::AbstractVector{SVector{3, T}}) where {L, T} 
-   Flm = basis.Flm
-   cache = basis.cache
 
    nX = length(Rs)
-   len = sizeY(L)
-   rt2 = sqrt(2) 
 
-   # x, y, z, r², s, c, Q = _get_temps(basis, nX, T)
-   x = acquire!(basis.cache, :x,  (nX, ),    T)
-   y = acquire!(basis.cache, :y,  (nX, ),    T)
-   z = acquire!(basis.cache, :z,  (nX, ),    T)
-   r² = acquire!(basis.cache, :r2, (nX, ),   T)
-   s = acquire!(basis.cache, :s,  (nX, L+1), T)
-   c = acquire!(basis.cache, :c,  (nX, L+1), T)
-   Q = acquire!(basis.cache, :Q,  (nX, sizeY(L)), T)
-   
+   # get some temporary arrays for x, y, z coordinates from the cache
+   x = acquire!(basis.cache, :x, (nX, ), T)
+   y = acquire!(basis.cache, :y, (nX, ), T)
+   z = acquire!(basis.cache, :z, (nX, ), T)
+
    @inbounds @simd ivdep for j = 1:nX
       rr = Rs[j] 
       xj, yj, zj = rr[1], rr[2], rr[3]
       x[j] = xj 
       y[j] = yj 
       z[j] = zj 
-      r²[j] = xj^2 + yj^2 + zj^2
+   end 
 
+   # allocate temporary arrays from an array cache 
+   temps = (r² = acquire!(basis.cache, :r2, (nX, ),   T),
+            s = acquire!(basis.cache, :s,  (nX, L+1), T),
+            c = acquire!(basis.cache, :c,  (nX, L+1), T),
+            Q = acquire!(basis.cache, :Q,  (nX, sizeY(L)), T), 
+            Flm = basis.Flm )
+
+   # the actual evaluation kernel 
+   solid_harmonics!(Z, Val{L}(), x, y, z, temps)
+
+   # release the temporary arrays back into the cache
+   release!(temps.x)
+   release!(temps.y)
+   release!(temps.z)
+   release!(temps.r²)
+   release!(temps.s)
+   release!(temps.c)
+   release!(temps.Q)
+
+   return Z 
+end 
+
+
+# ----- Computational kernel for `ZlmBasis`
+
+function solid_harmonics!(Z::AbstractMatrix, ::Val{L}, 
+            x::AbstractVector{T}, y::AbstractVector{T}, z::AbstractVector{T}, 
+            temps::NamedTuple 
+            ) where {L, T <: AbstractFloat}
+
+   nX = length(x)
+   len = sizeY(L)
+
+   r² = temps.r²
+   s = temps.s
+   c = temps.c
+   Q = temps.Q
+   Flm = temps.Flm
+
+   # some size checks to make sure the inbounds macro can be used safely. 
+   @assert length(y) == length(z) == nX 
+   @assert length(r²) >= nX
+   @assert size(Z, 1) >= nX && size(s, 1) >= nX  && size(c, 1) >= nX && size(Q, 1) >= nX
+   @assert size(Z, 2) >= len && size(Q, 2) >= len 
+   @assert size(s, 2) >= L+1 && size(c, 2) >= L+1
+
+   rt2 = sqrt(2) 
+   
+   @inbounds @simd ivdep for j = 1:nX
+      r²[j] = x[j]^2 + y[j]^2 + z[j]^2
       # c_m and s_m, m = 0 
       s[j, 1] = zero(T)    # 0 -> 1
       c[j, 1] = one(T)     # 0 -> 1
@@ -297,15 +277,7 @@ function evaluate!(Z::AbstractMatrix,
       end
    end
 
-   release!(x)
-   release!(y)
-   release!(z)
-   release!(r²)
-   release!(s)
-   release!(c)
-   release!(Q)
-
-   # finally generate an svector output 
-   return nothing 
+   return Z 
 end
    
+end 
