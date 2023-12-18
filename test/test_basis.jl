53cb513e4bc4cdb1e11ab8a5353815e83e2bc1ce
ACEsuit <christohortner@gmail.com>
2023-12-18 15:56:13 -0800

    draft evaluate_d_basis

--- test/test_basis.jl
+++ test/test_basis.jl
@@ -25,7 +25,7 @@ model = acemodel(; elements = elements,
                    order = 3, totaldegree = 10, 
                    Eref = Dict(:Si => -1.234, :O => -0.432))
 ace1 = model.potential                   
-uf_ace = UltraFastACE.uface_from_ace1(pot1; n_spl_points = 10_000)
+uf_ace = UltraFastACE.uface_from_ace1(ace1; n_spl_points = 10_000)
 
 ## ------------------------------------
 
@@ -60,15 +60,23 @@ LinearACEInnerBasis(rbasis, ybasis, abasis, aabasis,
       LinearACEInnerBasis(rbasis, ybasis, abasis, aabasis, meta, 
                            TSafe(ArrayPool(FlexArrayCache)))
 
-struct LinearACEBasis{NZ, INNER, PAIR}
+Base.length(ufb_in::LinearACEInnerBasis) = length(ufb_in.aabasis)
+
+struct LinearACEBasis{NZ, INNER}
    _i2z::NTuple{NZ, Int}
    ace_inner::INNER
-   pairbasis::PAIR
-   E0s::Dict{Int, Float64}
+   # pairbasis::PAIR
+   # E0s::Dict{Int, Float64}
    # ---------- 
    meta::Dict{String, Any}
    pool::TSafe{ArrayPool{FlexArrayCache}}
 end
+
+LinearACEBasis(_i2z, ace_inner, meta = Dict{String, Any}()) = 
+         LinearACEBasis(_i2z, ace_inner, meta, 
+                       TSafe(ArrayPool(FlexArrayCache)))
+
+Base.length(basis::LinearACEBasis) = sum(length, basis.ace_inner) 
 
 function inner_basis_from_ufpot(ufpot_in)
    AA_spec = ufpot_in.meta["AA_spec"]
@@ -82,7 +90,13 @@ function inner_basis_from_ufpot(ufpot_in)
 end
 
 function basis_from_ufpot(ufpot)
-   
+   NZ = length(ufpot.ace_inner)
+   t_inner = ntuple(iz -> inner_basis_from_ufpot(ufpot.ace_inner[iz]), NZ)
+   coeffs = ntuple(iz -> ufpot.ace_inner[iz].meta["AA_coeffs"], NZ)
+   meta = Dict{String, Any}(ufpot.meta...)
+   basis = LinearACEBasis(ufpot._i2z, t_inner, meta)
+   ps = (coeffs_inner = coeffs, )
+   return basis, ps 
 end
 
 function evaluate_basis(ufb_in::LinearACEInnerBasis, Rs, Zs)
@@ -108,18 +122,120 @@ function evaluate_basis(ufb_in::LinearACEInnerBasis, Rs, Zs)
    return AA
 end
 
+function evaluate(basis::LinearACEBasis, Rs, Zs, z0, ps::NamedTuple)
+   iz0 = UltraFastACE._z2i(basis, z0)
+   AA = evaluate_basis(basis.ace_inner[iz0], Rs, Zs)
+   return dot(AA, ps.coeffs_inner[iz0])
+end
+
+function _get_range(basis::LinearACEBasis, z0)
+   iz0 = UltraFastACE._z2i(basis, z0) 
+   lens_inner = length.(basis.ace_inner)
+   start = sum(lens_inner[1:iz0-1]) + 1
+   stop = sum(lens_inner[1:iz0])
+   return start:stop
+end
+
+function evaluate_basis(basis::LinearACEBasis, Rs, Zs, z0)
+   iz0 = UltraFastACE._z2i(basis, z0)
+   AA_iz0 = evaluate_basis(basis.ace_inner[iz0], Rs, Zs)
+   AA_all = zeros(eltype(AA_iz0), length(basis))
+   AA_all[_get_range(basis, z0)] = AA_iz0
+   release!(AA_iz0) 
+   return AA_all 
+end
+
+
+function evaluate_d_basis(ufb_in::LinearACEInnerBasis, Rs, Zs)
+   TF = eltype(eltype(Rs))
+   TV = eltype(Rs) 
+   rbasis = ufb_in.rbasis 
+   NZ = length(rbasis._i2z)
+   nX = length(Rs) 
+   
+   # embeddings    
+   Ez = UltraFastACE.embed_z(ufb_in, Rs, Zs)
+   Rn, ∂Rn = UltraFastACE.evaluate_ed(ufb_in, rbasis, Rs, Zs)
+   Zlm, ∂Zlm = UltraFastACE.evaluate_ylm_ed(ufb_in, Rs)
+
+   # because the pfwd implementation doesn't know about categorial 
+   # variables we need to create a zero-valued array for dEz 
+   ∂Ez = zeros(TV, size(Ez))
+
+   # pooling 
+   A, ∂A = Polynomials4ML.pfwd_evaluate(ufb_in.abasis, 
+                           (unwrap(Ez),  unwrap(Rn),  unwrap(Zlm)), 
+                           (unwrap(∂Ez), unwrap(∂Rn), unwrap(∂Zlm)))
+
+   # n correlations
+   AA, ∂AA = Polynomials4ML.pfwd_evaluate(ufb_in.aabasis, A, ∂A) 
+
+   # release the borrowed arrays 
+   release!(Zlm); release!(∂Zlm)
+   release!(Rn); release!(∂Rn)
+   release!(Ez); release!(∂Ez)
+   release!(A); release!(∂A) 
+
+   return AA, ∂AA
+end
+
+
+function evaluate_d_basis(basis::LinearACEBasis, Rs, Zs, z0)
+   iz0 = UltraFastACE._z2i(basis, z0)
+   AA_iz0, ∂AA_iz0 = evaluate_d_basis(basis.ace_inner[iz0], Rs, Zs)
+   AA_all = zeros(eltype(AA_iz0), length(basis))
+   ∂AA_all = zeros(eltype(∂AA_iz0), length(basis), size(∂AA_iz0, 2))
+   rg = _get_range(basis, z0)
+   AA_all[rg] = AA_iz0
+   ∂AA_all[rg, :] = ∂AA_iz0
+   release!(AA_iz0)
+   release!(∂AA_iz0) 
+   return AA_all, ∂AA_all
+end
+
+
 end
 
 ##
 
 Rs, Zs, z0 = rand_env()
-ufpot_in1 = uf_ace.ace_inner[1]
+iz0 = UltraFastACE._z2i(uf_ace, z0)
+ufpot_in1 = uf_ace.ace_inner[iz0]
 ufb_in1 = UFB.inner_basis_from_ufpot(ufpot_in1)
 coeffs = ufpot_in1.meta["AA_coeffs"]
 
 v1 = ACEbase.evaluate(ufpot_in1, Rs, Zs)
 AA = UFB.evaluate_basis(ufb_in1, Rs, Zs)
 v2 = dot(coeffs, AA)
+v1 ≈ v2
 
-length(ufb_in1.aabasis)
-length(AA)
+AA2, ∂AA2 = UFB.evaluate_d_basis(ufb_in1, Rs, Zs)
+
+using ForwardDiff
+using LinearAlgebra: I 
+
+Us = randn(SVector{3, Float64}, length(Rs))
+F = t -> UFB.evaluate_basis(ufb_in1, Rs + t * Us, Zs)
+adF = ForwardDiff.derivative(F, 0.0)
+dF = [ sum(dot(∂AA2[i, j], Us[j]) for j = 1:length(Rs)) for i = 1:length(AA) ]
+@show adF ≈ dF
+
+## 
+
+basis, ps = UFB.basis_from_ufpot(uf_ace)
+coeffs = vcat(ps.coeffs_inner...)
+AA_all = UFB.evaluate_basis(basis, Rs, Zs, z0)
+dot(coeffs, AA_all) ≈ v2
+
+AA_all2, ∂AA_all2 = UFB.evaluate_d_basis(basis, Rs, Zs, z0)
+AA_all2 ≈ AA_all
+
+Us = randn(SVector{3, Float64}, length(Rs))
+F = t -> UFB.evaluate_basis(basis, Rs + t * Us, Zs, z0)
+adF = ForwardDiff.derivative(F, 0.0)
+dF = [ sum(dot(∂AA_all2[i, j], Us[j]) for j = 1:length(Rs)) 
+        for i = 1:length(AA_all2) ]
+@show adF ≈ dF
+
+##
+
--- test/test_basis_2.jl
+++ test/test_basis_2.jl
@@ -118,11 +118,100 @@ function evaluate_basis(ufb_in::LinearACEInnerBasis, Rs, Zs)
    return AA
 end
 
+using ReverseDiff, Zygote
+
 function evaluate(ufbas::LinearACEBasis, Rs, Zs, z0, ps::NamedTuple)
-   iz0 = UltraFastACE._z2i(ufbas, z0)
-   AA = evaluate_basis(ufbas.ace_inner[iz0], Rs, Zs)
+   iz0, AA = Zygote.ignore() do 
+      iz0 = UltraFastACE._z2i(ufbas, z0)
+      ( iz0, evaluate_basis(ufbas.ace_inner[iz0], Rs, Zs))
+   end
    return dot(AA, ps.coeffs_inner[iz0])
 end
+
+
+# function evaluate_ed(ace::UFACE, Rs, Zs, z0)
+#    ∇φ = acquire!(ace.pool, :out_dEs, (length(Rs),), eltype(Rs))
+#    return evaluate_ed!(∇φ, ace, Rs, Zs, z0)
+# end
+
+# function evaluate_ed!(∇φ, ace::UFACE, Rs, Zs, z0)
+#    i_z0 = _z2i(ace, z0)
+#    ace_inner = ace.ace_inner[i_z0]
+#    φ, _ = evaluate_ed!(∇φ, ace_inner, Rs, Zs)
+#    add_evaluate_d!(∇φ, ace.pairpot, Rs, Zs, z0)
+#    φ += ace.E0s[z0] + evaluate(ace.pairpot, Rs, Zs, z0)
+#    return φ, ∇φ
+# end
+
+
+# function ACEbase.evaluate_ed!(∇φ, ace::UFACE_inner, Rs, Zs)
+#    TF = eltype(eltype(Rs))
+#    rbasis = ace.rbasis 
+#    NZ = length(rbasis._i2z)
+
+#    # embeddings    
+#    # element embedding  (there is no gradient)
+#    Ez = embed_z(ace, Rs, Zs)
+
+#    # radial embedding 
+#    Rn, dRn = evaluate_ed(ace, rbasis, Rs, Zs)
+
+#    # angular embedding 
+#    Zlm, dZlm = evaluate_ylm_ed(ace, Rs)
+
+#    # pooling 
+#    A = ace.abasis((Ez, Rn, Zlm))
+
+#    # n correlations - compute with gradient, do it in-place 
+#    ∂φ_∂A = acquire!(ace.pool, :∂A, size(A), TF)
+#    φ = evaluate_and_gradient!(∂φ_∂A, ace.aadot, A)
+   
+#    # backprop through A  =>  this part could be done more nicely I think
+#    ∂φ_∂Ez = BlackHole(TF) 
+#    # ∂φ_∂Ez = zeros(TF, size(Ez))
+#    ∂φ_∂Rn = acquire!(ace.pool, :∂Rn, size(Rn), TF)
+#    ∂φ_∂Zlm = acquire!(ace.pool, :∂Zlm, size(Zlm), TF)
+#    fill!(∂φ_∂Rn, zero(TF))
+#    fill!(∂φ_∂Zlm, zero(TF))
+#    P4ML._pullback_evaluate!((∂φ_∂Ez, unwrap(∂φ_∂Rn), unwrap(∂φ_∂Zlm)), 
+#                              unwrap(∂φ_∂A), 
+#                              ace.abasis, 
+#                              (unwrap(Ez), unwrap(Rn), unwrap(Zlm)); 
+#                              sizecheck=false)
+
+#    # backprop through the embeddings 
+#    # depending on whether there is a bottleneck here, this can be 
+#    # potentially implemented more efficiently without needing writing/reading 
+#    # (to be investigated where the bottleneck is)
+   
+#    # we just ignore Ez (hence the black hole)
+
+#    # backprop through Rn 
+#    # We already computed the gradients in the forward pass
+#    fill!(∇φ, zero(SVector{3, TF}))
+#    @inbounds for n = 1:size(Rn, 2)
+#       @simd ivdep for j = 1:length(Rs)
+#          ∇φ[j] += ∂φ_∂Rn[j, n] * dRn[j, n]
+#       end
+#    end
+
+#    # ... and Ylm 
+#    @inbounds for i_lm = 1:size(Zlm, 2)
+#       @simd ivdep for j = 1:length(Rs)
+#          ∇φ[j] += ∂φ_∂Zlm[j, i_lm] * dZlm[j, i_lm]
+#       end
+#    end
+
+#    # release the borrowed arrays 
+#    release!(Zlm); release!(dZlm)
+#    release!(Rn); release!(dRn)
+#    release!(Ez)
+#    release!(A)
+#    release!(∂φ_∂Rn); release!(∂φ_∂Zlm); release!(∂φ_∂A)
+
+#    return φ, ∇φ 
+# end
+
 
 end
 
@@ -144,12 +233,18 @@ v3 = evaluate(ufbas, Rs, Zs, z0, ps)
 
 ## 
 
-using ReverseDiff, Optimisers
+using Zygote, ReverseDiff, Optimisers, ForwardDiff
 
 pvec, _rest = destructure(ps)
-AA_ad = ReverseDiff.gradient(_p -> evaluate(ufbas, Rs, Zs, z0, _rest(_p)), pvec)
-AA1 = UFB.evaluate_basis(ufbas.ace_inner[1], Rs, Zs)
+@time AA_fd = ForwardDiff.gradient(_p -> evaluate(ufbas, Rs, Zs, z0, _rest(_p)), pvec)
+@time AA_rd = ReverseDiff.gradient(_p -> evaluate(ufbas, Rs, Zs, z0, _rest(_p)), pvec)
+@time AA_zy = Zygote.gradient(_ps -> evaluate(ufbas, Rs, Zs, z0, _ps), ps)
+@time AA1 = UFB.evaluate_basis(ufbas.ace_inner[1], Rs, Zs)
 AA2 = UFB.evaluate_basis(ufbas.ace_inner[2], Rs, Zs)
 AA_man = [ (iz0 == 1) * AA1; (iz0 == 2) * AA2 ]
 AA_ad == AA_man
 
+# unfortunately the performance of both reversediff and zygote are 
+# really aweful here. This is very sad.
+
+## 
